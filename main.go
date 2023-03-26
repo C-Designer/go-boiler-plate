5391310a38acdc11d2f71a0a1eaa06f6543e3ac1
우영창 <uyeongchang@uyeongchang-ui-MacBookPro.local>
2023-03-26 23:12:16 +0900

    Fix: dml- insert

--- main.go
+++ main.go
@@ -18,8 +18,7 @@ type User struct {
 	Email    string `json:"email"`
 }
 
-
-func DBConfig() {
+func DBConfig() *sql.DB {
 	db, err := sql.Open("mysql", "root:12341234@tcp(localhost:3306)/go_test")
 
 	if err != nil {
@@ -38,49 +37,55 @@ func DBConfig() {
 
 	}
 	fmt.Printf("query success %+v \n", res)
+
+	return db
 }
 
 func main() {
 
-	DBConfig()
+	db := DBConfig()
 
 	users := make(map[int]*User)
 
-	pk := 0
 	http.HandleFunc("/api/user", func(w http.ResponseWriter, r *http.Request) {
 		switch r.Method {
 
-			case http.MethodGet:
-				w.Header().Set("Content-Type", "application/json")
-				json.NewEncoder(w).Encode(users)
-				user.FindUser()
-				board.FindBoard()
-			case http.MethodPost:
-				// var user User
+		case http.MethodGet:
 
-				println(r.PostFormValue("test"))
-				// println(r.Body)
+			w.Header().Set("Content-Type", "application/json")
 
-				fmt.Printf(" req.body : %+v  ",r.PostForm)
+			json.NewEncoder(w).Encode(users)
 
-				body := r.PostForm
+			user.FindUser()
 
-				fmt.Print(body)
-				// json.NewDecoder(r.Body).Decode(&user)
-				pk = +pk + 1
-				// var ee User = User(body)
-				// users[pk] = User(body)
+			board.FindBoard()
 
-				w.Header().Set("Content-Type", "application/json")
-				json.NewEncoder(w).Encode(body)
-				// return
+		case http.MethodPost:
+
+			println(r.PostFormValue("test"))
+
+			fmt.Printf(" req.body : %+v  ", r.PostForm)
+
+			body := r.PostForm
+
+			query := `insert into User (email, name, age) values (?,?,?)`
+
+			stmp, _ := db.Prepare(query)
+
+			_, err := stmp.Exec(body.Get("email"), body.Get("name"), body.Get("age"))
+
+			if err != nil {
+				log.Fatal(err)
 			}
+
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(body)
+			// return
+		}
 	})
 
 	log.Fatal(http.ListenAndServe(":5050", nil))
 
 }
 
-//go 환경 셋팅
-//go 스타일 낭독
-//go arch
+//go 언어 설치하고 환경 설정 해야됨
